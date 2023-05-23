09cdf518ac319b75cefd9e7bdcdfb775ae125324
byiringiroscar <byiringoroscar@gmail.com>
2023-05-23 13:02:30 +0200

    First commit

---
+++ django_notification/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for django_notification project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_notification.settings')
+django.setup()
+from channels.auth import AuthMiddleware
+from notifications_app.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    'websocket': AuthMiddleware()
+})
+
